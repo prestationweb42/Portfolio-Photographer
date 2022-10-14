cc13ce3d2dd30ac635904044208854df824d61ce
danivent23122020 <dani.vent@orange.fr>
2022-10-14 22:20:01 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-// alert("toto");
+// add Event on multiple element
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+};
+
+// Preloading
+const loadingElement = document.querySelector("[data-loading]");
+//
+window.addEventListener("load", function () {
+    loadingElement.classList.add("loaded");
+    document.body.classList.remove("active");
+});
